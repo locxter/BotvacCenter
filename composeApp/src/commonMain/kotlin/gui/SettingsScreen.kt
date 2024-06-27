7041544e9d26545ef0e0ecd9e61b35a5eaab9109
locxter <54595101+locxter@users.noreply.github.com>
2024-06-27 22:34:56 +0200

    Made settings editable.

--- composeApp/src/commonMain/kotlin/gui/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/gui/SettingsScreen.kt
@@ -7,18 +7,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import gui.components.Dropdown
 import gui.components.Label
 import gui.components.Navigation
 import gui.components.Title
-import model.EStatus
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -31,31 +31,35 @@ fun SettingsScreen(
     Column(
         Modifier.fillMaxSize().verticalScroll(rememberScrollState()).padding(20.dp)
     ) {
+        var aliasInput by remember { mutableStateOf(alias) }
+        var addressInput by remember { mutableStateOf(address) }
+        var usernameInput by remember { mutableStateOf(address) }
+        var passwordInput by remember { mutableStateOf(address) }
         Navigation(onClick = {}, modifier = Modifier.padding(bottom = 10.dp), isBack = true) {
             Title("Settings")
         }
         Label("Alias:", modifier = Modifier.padding(bottom = 5.dp))
         OutlinedTextField(
-            value = alias,
-            onValueChange = { },
+            value = aliasInput,
+            onValueChange = { aliasInput = it },
             modifier = Modifier.fillMaxWidth().padding(bottom = 10.dp)
         )
         Label("Address:", modifier = Modifier.padding(bottom = 5.dp))
         OutlinedTextField(
-            value = address,
-            onValueChange = { },
+            value = addressInput,
+            onValueChange = { addressInput = it },
             modifier = Modifier.fillMaxWidth().padding(bottom = 10.dp)
         )
         Label("Username:", modifier = Modifier.padding(bottom = 5.dp))
         OutlinedTextField(
-            value = username,
-            onValueChange = { },
+            value = usernameInput,
+            onValueChange = { usernameInput = it },
             modifier = Modifier.fillMaxWidth().padding(bottom = 10.dp)
         )
         Label("Password:", modifier = Modifier.padding(bottom = 5.dp))
         OutlinedTextField(
-            value = password,
-            onValueChange = { },
+            value = passwordInput,
+            onValueChange = { passwordInput = it },
             modifier = Modifier.fillMaxWidth()
         )
         Spacer(Modifier.weight(1f))
