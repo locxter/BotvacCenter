1c6cd06ab610ae7a7d431fd73d1ccb572aaba719
locxter <54595101+locxter@users.noreply.github.com>
2024-07-23 20:11:54 +0200

    Added a really ugly application icon.

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,10 +1,15 @@
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import botvaccenter.composeapp.generated.resources.Res
+import botvaccenter.composeapp.generated.resources.icon
+import org.jetbrains.compose.resources.painterResource
 
 fun main() = application {
+    val icon = painterResource(Res.drawable.icon)
     Window(
         onCloseRequest = ::exitApplication,
         title = "BotvacCenter",
+        icon = icon
     ) {
         App()
     }
