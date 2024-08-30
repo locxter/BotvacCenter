a91fd71091bd4c2d7feff64baee855c9ec48b158
locxter <54595101+locxter@users.noreply.github.com>
2024-08-30 16:05:01 +0200

    Added collision box indicator to MapVisualiation.

--- composeApp/src/commonMain/kotlin/com/github/locxter/botvaccenter/gui/components/MapVisualisation.kt
+++ composeApp/src/commonMain/kotlin/com/github/locxter/botvaccenter/gui/components/MapVisualisation.kt
@@ -10,10 +10,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PointMode
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.DrawStyle
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.AnnotatedString
@@ -151,6 +154,15 @@ fun MapVisualization(
                         (tempBotvac.location.x * scalingRatio),
                         ((yRange - tempBotvac.location.y) * scalingRatio)
                     )
+                )
+                drawCircle(
+                    color = robotColor,
+                    radius = 250 * scalingRatio,
+                    center = Offset(
+                        (tempBotvac.location.x * scalingRatio),
+                        ((yRange - tempBotvac.location.y) * scalingRatio)
+                    ),
+                    style = Stroke(width = max(maxRange * 0.0025f * scalingRatio, 1.0f))
                 )
                 drawLine(
                     color = robotColor,
